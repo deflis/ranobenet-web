b18100d91a9118334974031fc5c54c2e6ee23a28
deflis <deflis@gmail.com>
2022-02-16 19:51:21 +0900

    テストを実行できるようにする

--- jest.config.ts
+++ jest.config.ts
@@ -1,33 +1,18 @@
-import { pathsToModuleNameMapper } from 'ts-jest/utils';
-import type { Config } from '@jest/types';
-import { compilerOptions } from './tsconfig.json';
+const nextJest = require('next/jest');
 
-const config: { projects: Config.InitialOptions[] } = {
-  projects: [
-    {
-      setupFiles: ['<rootDir>/test/jest.setup.js'],
-      testPathIgnorePatterns: ['<rootDir>/server'],
-      transform: {
-        '^.+\\.tsx$': 'babel-jest',
-        '^.+\\.ts$': 'ts-jest',
-      },
-      moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
-        ...pathsToModuleNameMapper(compilerOptions.paths, {
-          prefix: '<rootDir>/',
-        }),
-      },
-    },
-    {
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      testMatch: ['<rootDir>/server/test/**/*.ts'],
-      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: '<rootDir>/',
-      }),
-    },
-  ],
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
 };
 
-export default config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(customJestConfig);
