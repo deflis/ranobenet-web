44a9fce88186aba4de09df932a083eeea9028507
deflis <deflis@gmail.com>
2022-02-21 22:19:53 +0900

    CSS修正

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,15 @@
 const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
-    content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}'
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    safelist: ["html", "body"]
-  }
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    safelist: ['html', 'body'],
+  },
 ];
 module.exports = {
   plugins: [
     'postcss-import',
+    'tailwindcss/nesting',
     'tailwindcss',
     ['postcss-preset-env', { stage: 1 }],
     // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
