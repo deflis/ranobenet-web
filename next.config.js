6bab1f72f5258d26a1902b79dbd1edab102d071d
deflis <deflis@gmail.com>
2022-02-23 01:39:48 +0900

    PWAの設定を修正

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   pwa: {
+    dest: 'public',
     runtimeCaching,
   },
 };
