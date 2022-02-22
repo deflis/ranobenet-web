3aaeb855fb5e1644934da62249692025885c2eca
deflis <deflis@gmail.com>
2022-02-23 04:47:36 +0900

    app.tsxをFunctionalComponent化

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,13 @@ import { GlobalContainer } from '~/components/templates/global/GlobalContainer';
 import { Provider } from 'jotai';
 import { useAuth } from '~/utils/firebase/auth';
 
-export default function MyApp(props: AppProps) {
-  return (
-    <Provider>
-      <InnerApp {...props} />
-    </Provider>
-  );
-}
+const App = (props: AppProps) => (
+  <Provider>
+    <InnerApp {...props} />
+  </Provider>
+);
 
-function InnerApp({ Component, pageProps }: AppProps) {
+const InnerApp = ({ Component, pageProps }: AppProps) => {
   useAuth();
 
   return (
@@ -20,4 +18,6 @@ function InnerApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </GlobalContainer>
   );
-}
+};
+
+export default App;
