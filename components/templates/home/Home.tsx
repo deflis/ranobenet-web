309c9bb3a0b0705b0aa1eb7aa45a239ac1b07851
deflis <deflis@gmail.com>
2022-02-24 04:49:02 +0900

    トップページを変更

--- components/templates/home/Home.tsx
+++ components/templates/home/Home.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import NextLink from 'next/link';
 import { useFirebaseUser } from '~/utils/firebase/auth';
 import { NextLinkButton } from '~/components/atoms/common/Button';
 import { pageLogin, pageNovels, pageUsers } from '~/utils/path';
@@ -15,8 +14,8 @@ export const Home = () => {
         <title>らのべねっと</title>
       </Head>
 
-      <NextLink href={pageUsers()}>ユーザー一覧</NextLink>
-      <NextLink href={pageNovels()}>小説一覧</NextLink>
+      <NextLinkButton href={pageUsers()}>ユーザー一覧</NextLinkButton>
+      <NextLinkButton href={pageNovels()}>小説一覧</NextLinkButton>
       {!user && <NextLinkButton href={pageLogin()}>ログイン</NextLinkButton>}
       {user && <NextLinkButton href={pageEditUserMe()}>ユーザー情報を編集する</NextLinkButton>}
       {user && <NextLinkButton href={pageEditNovelCreate()}>新しい小説を書く</NextLinkButton>}
