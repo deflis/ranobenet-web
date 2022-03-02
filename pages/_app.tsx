a3f4f8d249b61c89d396f11ff186055ac715fc7b
deflis <deflis@gmail.com>
2022-03-03 00:05:31 +0900

    toast追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
-import '~/modules/utils/globals.css';
 import { Provider, useAtomValue } from 'jotai';
 import { useAuthStateListener } from '~/modules/utils/firebase/auth';
 import { queryClientAtom } from 'jotai/query';
 import { Hydrate, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { ToastContainer } from 'react-toastify';
+
+import '~/modules/utils/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export type PropsDehydratedState = {
   dehydratedState: unknown;
@@ -25,6 +28,17 @@ const InnerApp = ({ Component, pageProps }: AppProps) => {
       <Hydrate state={(pageProps as Partial<PropsDehydratedState>)?.dehydratedState}>
         <Component {...pageProps} />
         <ReactQueryDevtools />
+        <ToastContainer
+          position='top-center'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Hydrate>
     </QueryClientProvider>
   );
