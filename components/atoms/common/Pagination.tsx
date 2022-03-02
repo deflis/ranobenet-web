1146356f3e491b1489e1ebc7459a5b761a6183f2
deflis <deflis@gmail.com>
2022-03-02 18:13:59 +0900

    アイコンを導入

--- components/atoms/common/Pagination.tsx
+++ components/atoms/common/Pagination.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import NextLink from 'next/link';
 import { container, current, dots, nav, next, page, previous } from './Pagination.module.css';
 import { UrlObject } from 'url';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
 declare type Url = string | UrlObject;
 
@@ -68,36 +69,12 @@ export const Pagination: React.FC<{ pagedList: PagedList; createHref: (page: num
         {currentPage > 1 ? (
           <NextLink href={createHref(currentPage - 1)}>
             <a className={previous}>
-              <svg
-                className='h-5 w-5'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                aria-hidden='true'
-              >
-                <path
-                  fill-rule='evenodd'
-                  d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
-                  clip-rule='evenodd'
-                />
-              </svg>
+              <HiChevronLeft />
             </a>
           </NextLink>
         ) : (
           <span className={previous}>
-            <svg
-              className='h-5 w-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fill-rule='evenodd'
-                d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
-                clip-rule='evenodd'
-              />
-            </svg>
+            <HiChevronLeft />
           </span>
         )}
         {pagination.map((pageNumber, i) => (
@@ -118,36 +95,12 @@ export const Pagination: React.FC<{ pagedList: PagedList; createHref: (page: num
         {currentPage <= totalPages ? (
           <NextLink href={createHref(currentPage + 1)}>
             <a className={next}>
-              <svg
-                className='h-5 w-5'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                aria-hidden='true'
-              >
-                <path
-                  fill-rule='evenodd'
-                  d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
-                  clip-rule='evenodd'
-                />
-              </svg>
+              <HiChevronRight />
             </a>
           </NextLink>
         ) : (
           <span className={next}>
-            <svg
-              className='h-5 w-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fill-rule='evenodd'
-                d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
-                clip-rule='evenodd'
-              />
-            </svg>
+            <HiChevronRight />
           </span>
         )}
       </nav>
