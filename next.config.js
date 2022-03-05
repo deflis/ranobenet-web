c22578d1f8136f6b7ba0ec722457d86faf27e690
deflis <deflis@gmail.com>
2022-03-05 21:10:48 +0900

    MDXサポートを追加

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // PWA
   reactStrictMode: true,
   pwa: {
     dest: 'public',
     runtimeCaching,
   },
+  // MDX
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA(withMDX(nextConfig));
