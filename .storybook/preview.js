dc20eefaecb259f6d307d0a62a1d46054271524f
deflis <deflis@gmail.com>
2022-05-11 22:59:52 +0900

    storybook導入

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import '../modules/utils/globals.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  nextRouter: {
+    query: {
+      foo: 'this-is-a-global-override',
+    },
+  },
+};
